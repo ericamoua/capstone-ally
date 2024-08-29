4faa8cf3dcf75157819156eb907cf9fe9d2c2a2e
Rf2025 <165812829+Rf2025@users.noreply.github.com>
2024-08-29 14:48:29 -0400

    changed the file paths

--- src/App.jsx
+++ src/App.jsx
@@ -12,9 +12,9 @@ function App() {
   <div className='App'>
     <Nav/>
     <Routes>
-      <Route path="/" element={<Home/>}/>
+      {/* <Route path="/" element={<Home/>}/>
       <Route path="/login" element={<Login/>}/>
-      <Route path="/register" element={<Register/>}/>
+      <Route path="/register" element={<Register/>}/> */}
     </Routes>
     <Footer/>
   </div>
