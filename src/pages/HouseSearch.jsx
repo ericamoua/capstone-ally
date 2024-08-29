e940db13355b5a0195eebe34893467828ce1d764
katyamar22 <katyamarcam@gmail.com>
2024-08-29 13:45:01 -0400

    Made search component for finding houses.

---
+++ src/pages/HouseSearch.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ListingSearch from '../components/ListingSearch';
+
+const ListingsPage = () => {
+  return (
+    <div>
+      <ListingSearch />
+    </div>
+  )
+}
+
+export default ListingsPage
