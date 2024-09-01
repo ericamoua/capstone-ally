23e8deee33cec749cc2e7cc0fa33610595ec2c04
EricaMoua <erica.moua568@gmail.com>
2024-09-01 17:44:52 -0400

    "ui update for home page"

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import banner from '../styles/home.module.css';
 
 const Banner = ({ learnMoreLink }) => {
     return (
-        <div className="banner">
+        <div className={banner.banner}>
             <p>Find your dream home, click below for more</p>
             <a href={learnMoreLink} className="learn-more">Click here</a>
         </div>
