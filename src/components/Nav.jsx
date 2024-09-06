b11bbf869fae86b50c2de584f11f25afc08cde59
EricaMoua <erica.moua568@gmail.com>
2024-09-05 20:42:31 -0400

    pushing nav changes

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,10 +4,14 @@ import Logo from '../assets/logo-2.png'
 
 
 function Navbar() {
-  const [menuToggle, setMenuToggle] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const handleMenuClick = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
 
   return (
-    <>
+    <header>
       <nav id="navbar" className="">
         <div className="nav-wrapper">
           <div className="logo">
@@ -21,19 +25,21 @@ function Navbar() {
           </ul>
         </div>
       </nav>
-      <div className="menuIcon">
+      <div className="menuIcon" onClick={handleMenuClick}>
         <span className="icon icon-bars"></span>
         <span className="icon icon-bars overlay"></span>
       </div>
-      <div className="overlay-menu">
-        <ul id="menu">
-          <li><a href="#home">Home</a></li>
-          <li><a href="#services">Services</a></li>
-          <li><a href="#about">About</a></li>
-          <li><a href="#contact">Contact</a></li>
-        </ul>
-      </div>
-    </>
+      {isMenuOpen && (
+        <div className="overlay-menu">
+          <ul id="menu">
+            <li><a href="#home">Home</a></li>
+            <li><a href="#services">Services</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#contact">Contact</a></li>
+          </ul>
+        </div>
+      )}
+    </header>
   );
 }
 
