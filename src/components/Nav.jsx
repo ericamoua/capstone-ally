43a8ef537ca46192c65ab30107085cf41d128fe4
EricaMoua <erica.moua568@gmail.com>
2024-09-06 02:05:21 -0400

    "hamburger nav"

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,10 +1,15 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import '../styles/Navbar.css';
 import Logo from '../assets/logo-2.png'
 
 
 function Navbar() {
   const [menuToggle, setMenuToggle] = useState(false);
+
+  const handleMenuToggle = () => {
+    console.log(menuToggle);
+    setMenuToggle(!menuToggle);
+  }
 
   return (
     <>
@@ -16,25 +21,28 @@ function Navbar() {
           <ul id="menu">
             <li><a href="/">Home</a></li>
             <li><a href="/search">Find Your Home</a></li>
-            <li><a href="/resource">Contact And Resources</a></li>
+            <li><a href="/resource">Contact</a></li>
             <li><a href="/login">Login</a></li>
           </ul>
         </div>
       </nav>
-      <div className="menuIcon">
+      <div className="menuIcon" onClick={handleMenuToggle}>
         <span className="icon icon-bars"></span>
         <span className="icon icon-bars overlay"></span>
       </div>
-      <div className="overlay-menu">
-        <ul id="menu">
-          <li><a href="#home">Home</a></li>
-          <li><a href="#services">Services</a></li>
-          <li><a href="#about">About</a></li>
-          <li><a href="#contact">Contact</a></li>
-        </ul>
-      </div>
+
+      {menuToggle && (
+        <div className="overlay-menu">
+          <ul id="menu">
+            <li><a href="/">Home</a></li>
+            <li><a href="/search">Find Your Home</a></li>
+            <li><a href="/resource">Contact</a></li>
+            <li><a href="/login">Login</a></li>
+          </ul>
+        </div>
+      )}
     </>
   );
 }
 
-export default Navbar;
+export default Navbar; 
