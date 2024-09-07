4d8c233b624cfb155db2da3fbc76739581b77942
katyamar22 <katyamarcam@gmail.com>
2024-09-06 20:21:23 -0400

    removed carousel.

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -5,6 +5,7 @@ import Logo from '../assets/logo-2.png'
 
 function Navbar() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [menuToggle, setMenuToggle] = useState(false);
 
   const handleMenuClick = () => {
     setIsMenuOpen(!isMenuOpen);
@@ -30,23 +31,6 @@ function Navbar() {
           </ul>
         </div>
       </nav>
-<<<<<<< HEAD
-      <div className="menuIcon" onClick={handleMenuClick}>
-        <span className="icon icon-bars"></span>
-        <span className="icon icon-bars overlay"></span>
-      </div>
-      {isMenuOpen && (
-        <div className="overlay-menu">
-          <ul id="menu">
-            <li><a href="#home">Home</a></li>
-            <li><a href="#services">Services</a></li>
-            <li><a href="#about">About</a></li>
-            <li><a href="#contact">Contact</a></li>
-          </ul>
-        </div>
-      )}
-    </header>
-=======
       <div className="menuIcon" onClick={handleMenuToggle}>
         <span className="icon icon-bars"></span>
         <span className="icon icon-bars overlay"></span>
@@ -62,8 +46,7 @@ function Navbar() {
           </ul>
         </div>
       )}
-    </>
->>>>>>> 21d6498cf8eed03729a7b70f8b46631bcb384841
+    </header>
   );
 }
 
